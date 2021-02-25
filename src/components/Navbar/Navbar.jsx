4dad8ad9a845673ff91bfc3c411bcb4961e1796d
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-02-25 13:17:31 +0300

    SN-014 add style css module

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import style from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav className='nav'>
-      <div>Profile</div>
-      <div>Messages</div>
-      <div>News</div>
-      <div>Music</div>
-      <div>Settings</div>
+    <nav className={style.nav}>
+      <div className={style.items}>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+      </div>
     </nav>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={style.content}>
       <div>
         <img
           src='https://upload.wikimedia.org/wikipedia/commons/6/6c/Herjangsfjorden_%26_Ofotfjorden%2C_wide%2C_2009_09.jpg'
@@ -10,19 +11,19 @@ const Profile = () => {
         />
       </div>
       <div>{/* <p>Main Content</p> */}</div>
-      <div className='avatar_and_info_block'>
-        <div className='avatar_image'>
+      <div className={style.avatar_and_info_block}>
+        <div className={style.avatar_image}>
           <img
             src='https://p7.hiclipart.com/preview/7/618/505/avatar-icon-fashion-men-vector-avatar.jpg'
             alt='avatar_image'
           />
         </div>
-        <div className='profile_info'>Ava + description</div>
+        <div className={style.profile_info}>Ava + description</div>
       </div>
-      <div className='post_area_block'>
+      <div className={style.post_area_block}>
         <div>My Posts</div>
         <div>New Post</div>
-        <div>
+        <div className={style.item}>
           <div>Post 1</div>
           <div>Post 2</div>
           <div>Post 3</div>
