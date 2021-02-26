83a542c0682a32d44a518dde4c935f63561d2b7b
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-02-26 11:26:15 +0300

    quick fix images

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import style from "./Profile.module.css";
+import wideImage from "../../assets/images/wideImage.jpg";
+import defaultAvatar from "../../assets/images/defaultAvatar.jpg";
 import MyPostsComponent from "./My_Posts_Module/MyPostsComponent";
 
 const Profile = () => {
   return (
     <div className={style.content}>
       <div>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Herjangsfjorden_%26_Ofotfjorden%2C_wide%2C_2009_09.jpg"
-          alt="wide_image"
-        />
+        <img src={wideImage} alt="wide_image" />
       </div>
       <div>{/* <p>Main Content</p> */}</div>
       <div className={style.avatar_and_info_block}>
         <div className={style.avatar_image}>
-          <img
-            src="https://p7.hiclipart.com/preview/7/618/505/avatar-icon-fashion-men-vector-avatar.jpg"
-            alt="avatar_image"
-          />
+          <img src={defaultAvatar} alt="avatar_image" />
         </div>
         <div className={style.profile_info}>Ava + description</div>
       </div>
