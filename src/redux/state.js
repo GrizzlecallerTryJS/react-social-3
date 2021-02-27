9582deac6b2ca26f71f58b84fa6e74e3b91124da
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-02-28 00:00:33 +0300

    SN_029 done

---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+import defaultImage from "../assets/images/defaultImage.png";
+
+let state = {
+  messagesPage: {
+    users: [
+      { id: 1, name: "Dima" },
+      { id: 2, name: "Yana" },
+      { id: 3, name: "Marina" },
+      { id: 4, name: "Sasha" },
+    ],
+    messages: [
+      { id: 1, message: "Lorem ipsum dolor sit amet, consectetur." },
+      {
+        id: 2,
+        message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      },
+      { id: 3, message: "Lorem ipsum dolor sit." },
+      { id: 4, message: "Lorem ipsum dolor sit amet." },
+    ],
+  },
+  profilePage: {
+    posts: [
+      { id: 1, message: "BEEP", image: defaultImage },
+      { id: 2, message: "BOOP", image: defaultImage },
+    ],
+  },
+};
+
+export default state;
