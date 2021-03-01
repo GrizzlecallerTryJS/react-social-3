de3caace146114ed6ffae044b4ca36597983c473
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-03-01 18:50:34 +0300

    SN-035 done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import "./index.css";
-import render from "./render";
 import state from "./redux/state";
+import React from "react";
+import ReactDOM from "react-dom";
 
-render(state);
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import {
+  addPost,
+  updateNewPostText,
+  zeroingMessage,
+  subscribe,
+} from "./redux/state";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+let rendenTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App
+          state={state}
+          addPost={addPost}
+          updateNewPostText={updateNewPostText}
+          zeroingMessage={zeroingMessage}
+        />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+rendenTree(state);
+
+subscribe(rendenTree);
+
+export default rendenTree;
