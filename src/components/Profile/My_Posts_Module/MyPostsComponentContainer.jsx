880d3f95524552c014f640e593261238e2e1f125
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-03-06 14:17:48 +0300

    SN-045 done

--- src/components/Profile/My_Posts_Module/MyPostsComponentContainer.jsx
+++ src/components/Profile/My_Posts_Module/MyPostsComponentContainer.jsx
@@ -2,52 +2,28 @@ import React from "react";
 import MyPostsComponent from "./MyPostsComponent";
 import { addPostAC, onPostChangeAC } from "../../../redux/profilePageReducer";
 
-import {connect} from "react-redux";
-
-/*const MyPostsComponentContainer = () => {
-  return (
-    <ContextProvider.Consumer>
-      {(state) => {
-        debugger;
-        let store = state.getState().profilePageReducer;
-
-        let addPost = () => {
-          state.dispatch(addPostAC());
-        };
-
-        let onPostChange = (text) => {
-          state.dispatch(onPostChangeAC(text));
-        };
-
-        return (
-          <MyPostsComponent
-            state={store}
-            addPost={addPost}
-            onPostChange={onPostChange}
-          />
-        );
-      }}
-    </ContextProvider.Consumer>
-  );
-};*/
+import { connect } from "react-redux";
 
 let mapStateToProps = (state) => {
-  return (
-      {
-        state: state.profilePageReducer,
-      }
-  )
-}
+  return {
+    state: state.profilePageReducer,
+  };
+};
 
 let mapDispatchToProps = (dispatch) => {
-  return (
-      {
-        addPost: () => {dispatch(addPostAC())},
-        onPostChange: (text) => {dispatch(onPostChangeAC(text))}
-      }
-  )
-}
+  return {
+    addPost: () => {
+      dispatch(addPostAC());
+    },
+    onPostChange: (text) => {
+      dispatch(onPostChangeAC(text));
+    },
+  };
+};
 
-let MyPostsComponentContainer = connect(mapStateToProps, mapDispatchToProps)(MyPostsComponent)
+let MyPostsComponentContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MyPostsComponent);
 
 export default MyPostsComponentContainer;
