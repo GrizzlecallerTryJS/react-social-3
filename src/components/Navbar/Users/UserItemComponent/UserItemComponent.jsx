1cddcceb20e22d80159091ccb2466fadb1846b17
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-03-14 22:11:04 +0300

    SN-062 done

--- src/components/Navbar/Users/UserItemComponent/UserItemComponent.jsx
+++ src/components/Navbar/Users/UserItemComponent/UserItemComponent.jsx
@@ -3,21 +3,25 @@ import defaultAvatar from "../../../../assets/images/defaultAvatar.jpg";
 import UserItem from "./UserItem/UserItem";
 
 let UserItemComponent = (props) => {
-  let followButton = (id) => {
-    props.followButton(id);
+  let setFollowStatus = (id, status) => {
+    props.setFollowStatus(id, status);
   };
 
   return props.users.map((u) => {
+    let buttonPresentStatus = () => {
+      return u.followed ? "unFollow" : "Follow";
+    };
     return (
       <div>
         <UserItem
           photosSmall={u.photos.small ? u.photos.small : defaultAvatar}
           name={u.name}
           status={u.status}
-          followButton={() => followButton(u.id)}
+          setFollowStatus={() => setFollowStatus(u.id, u.followed)}
           key={u.id}
           id={u.id}
           followStatus={u.followed}
+          buttonPresentStatus={buttonPresentStatus}
         />
       </div>
     );
