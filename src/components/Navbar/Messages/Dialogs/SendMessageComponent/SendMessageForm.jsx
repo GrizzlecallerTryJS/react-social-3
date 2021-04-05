d70142e4ba08024362a45b2caa300bcea6459af1
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-04-05 19:48:16 +0300

    fix_forns done

--- src/components/Navbar/Messages/Dialogs/SendMessageComponent/SendMessageForm.jsx
+++ src/components/Navbar/Messages/Dialogs/SendMessageComponent/SendMessageForm.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const SendMessageForm = (props) => {
-  const { register, handleSubmit, reset, errors } = useForm({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
     mode: "onSubmit",
   });
   const onSubmit = (data) => {
@@ -10,20 +15,21 @@ const SendMessageForm = (props) => {
     reset();
   };
 
+  const maxLength255 = 255;
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="sendMessageForm"></label>
         <textarea
           placeholder="type here"
-          name="sendMessageForm"
-          ref={register({ required: true, minLength: 10 })}
+          {...register("sendMessageForm", { required: true, maxLength: 255 })}
         />
         <div>
           {errors.sendMessageForm?.type === "required" &&
             "Your input is required"}
-          {errors.sendMessageForm?.type === "minLength" &&
-            "Min length is minLength symbols"}
+          {errors.sendMessageForm?.type === "maxLength" &&
+            `Max length is ${maxLength255} symbols`}
         </div>
       </div>
       <div>
